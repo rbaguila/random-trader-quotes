d32ccf7c84c99957b174e5df2787fc5a5087ddcc
Roinand Aguila <rbaguila1@up.edu.ph>
2021-04-29 13:23:23 +0800

    scrape trader quotes and update the App.js file

--- src/App.js
+++ src/App.js
@@ -1,44 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter, faTumblr, faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter, faTumblr } from '@fortawesome/free-brands-svg-icons'
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import quotes from './quotes'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="big-container">
-        <h1>Trader's Quotes</h1>
-        <div id="wrapper">
-          <div id="quote-box">
-            <div className="quote-text">
-              <FontAwesomeIcon icon={faQuoteLeft} /> Sample sample sample sample<span id="text"></span>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentQuote: 'Sample',
+      currentAuthor: 'Roi Aguila'
+    }
+  }
+
+  getRandomQuote(){
+    return quotes.getQuotes[
+      Math.floor(Math.random() * quotes.getQuotes.length)
+    ];
+  }
+
+  getQuote(){
+    let randomQuote = this.getRandomQuote();
+
+    const currentQuote = randomQuote.quote;
+    const currentAuthor = randomQuote.author;
+
+    this.setState({currentQuote, currentAuthor})
+  }
+
+  componentDidMount(){
+    this.getQuote();
+  }
+
+  render() {
+    const {currentQuote, currentAuthor} = this.state;
+    return (
+      <div className="App">
+        <div className="big-container">
+          <h1>Trader's Quotes</h1>
+          <div id="wrapper">
+            <div id="quote-box">
+
+              <div className="quote-text">
+                <FontAwesomeIcon icon={faQuoteLeft} /> {currentQuote}<span id="text"></span>
+              </div>
+
+              <div className="quote-author">
+                - <span id="author">{currentAuthor}</span>
+              </div>
+              <div className="buttons">
+                <a
+                 className="button" id="tweet-quote" title="Tweet this quote!"
+                 target="_top" href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(currentQuote)}\n -${encodeURIComponent(currentAuthor)}`}
+                >
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+                <a
+                  className="button" id="tumblr-quote" title="Post this quote on tumblr!"
+                  target="_top" href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=${encodeURIComponent(currentAuthor)}&content=${encodeURIComponent(currentQuote)}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`}
+                >
+                  <FontAwesomeIcon icon={faTumblr} />
+                </a>
+                <button className="button" id="new-quote" onClick={() => this.getQuote()}>New quote</button>
+              </div>
             </div>
-            <div className="quote-author">
-              - <span id="author"> Roi Aguila</span>
-            </div>
-            <div className="buttons">
-              <a className="button" id="facebook-quote" title="Post this quote on facebook!" target="_blank">
-                <FontAwesomeIcon icon={faFacebook} />
-              </a>
-              <a className="button" id="tweet-quote" title="Tweet this quote!" target="_blank">
-                <FontAwesomeIcon icon={faTwitter} />
-              </a>
-              <a className="button" id="tumblr-quote" title="Post this quote on tumblr!" target="_blank">
-                <FontAwesomeIcon icon={faTumblr} />
-              </a>
-              <button className="button" id="new-quote">New quote</button>
-            </div>
+            <div className="footer"> by RAguila</div>
           </div>
-          <div className="footer"> by RAguila</div>
-        </div>
 
+        </div>
+        <footer>
+          <a href='https://github.com/rbaguila'>RAguila</a>
+        </footer>
       </div>
-      <footer>
-        <a href='https://github.com/rbaguila'>RAguila</a>
-      </footer>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
---
+++ src/quotes.js
@@ -0,0 +1,71 @@
+exports.getQuotes = [
+{"id":"1","author":"Antonio Porchia","quote":"I know what I have given you...I do not know what you have received."},
+{"id":"2","author":"Michael M. Lewis","quote":"The men on the trading floor may not have been to school, but they have Ph.D.'s in man's ignorance."}
+,
+{"id":"3","author":"Yvan Byeajee","quote":"Trading doesn't just reveal your character, it also builds it if you stay in the game long enough."},
+{"id":"4","author":"Yvan Byeajee","quote":"When you learn to let go of the need to be right, being wrong gradually lose its power to disturb you."},
+{"id":"5","author":"Yvan Byeajee","quote":"In order to succeed, you first have to be willing to experience failure."},
+{"id":"6","author":"Yvan Byeajee","quote":"Reaching any goal in trading requires specific domain knowledge and technical skills. But then, after that, it's all mindset management. Yet most people ignore that 'they automatically think they have that last part all figured out, and it's a mistake."}
+,
+{"id":"7","author":"Yvan Byeajee","quote":"The process by which one accumulates money is so simple, yet so hard to implement for most."},
+{"id":"8","author":"Noor Alasadi","quote":"The lower your body gets the higher your jump will be."},
+{"id":"9","author":"David Sikhosana","quote":"Losses are necessary, as long as they are associated with a technique to help you learn from them"},
+{"id":"10","author":"Vivek Nair","quote":"Don't make friends with trend, make friends with each candlestick"},
+{"id":"11","author":"Adam Harris","quote":"The key is to develop a case where the probabilities of the market going in your favour is greater than it going against you."},
+{"id":"12","author":"Debabrata (David) Das","quote":"A stock screening feature is then used to find the leading stocks within the leading sectors."},
+{"id":"13","author":"Akiroq Brost","quote":"If you have to compromise your ethics to get it, then it is not worth having."},
+{"id":"14","author":"Jim Paul","quote":"Speculating (and this includes investing and trading) is the only human endeavor in which what feels good is the right thing to do."},
+{"id":"15","author":"Debabrata (David) Das","quote":"Buying stocks when the broader overall market is trending up is a good idea, for long positions. The trend will provide a tailwind for the stock prices to move higher."},
+{"id":"16","author":"rasarab","quote":"Trade the market for what it is, not for what you trust it to be"},
+{"id":"17","author":"Olawale daniel","quote":"In trading, you can't stop loss from happening but you can avoid big loss by using stop loss in your trades."},
+{"id":"18","author":"Warren Buffett","quote":"For investors as a whole, returns decrease as motion increases."},
+{"id":"19","author":"Alexander Elder","quote":"You can be free. You can live and work anywhere in the world. You can be independent from routine and not answer to anybody."},
+{"id":"20","author":"Nicolas Darvas","quote":"I believe in analysis and not forecasting."},
+{"id":"21","author":"Van K. Tharp","quote":"A peak performance trader is totally committed to being the best and doing whatever it takes to be the best. He feels totally responsible for whatever happens and thus can learn from mistakes. These people typically have a working business plan for trading because they treat trading as a business."},
+{"id":"22","author":"Warren Buffett","quote":"We simply attempt to be fearful when others are greedy and to be greedy only when others are fearful."},
+{"id":"23","author":"Randy McKay","quote":"When I get hurt in the market, I get the hell out. It doesn't matter at all where the market is trading. I just get out, because I believe that once you're hurt in the market, your decisions are going to be far less objective than they are when you're doing well' If you stick around when the market is severely against you, sooner or later they are going to carry you out."}
+,
+{"id":"24","author":"Victor Sperandeo","quote":"The key to trading success is emotional discipline. If intelligence were the key, there would be a lot more people making money trading' I know this will sound like a clich', but the single most important reason that people lose money in the financial markets is that they don't cut their losses short."}
+,
+{"id":"25","author":"William O'Neil","quote":"What seems too high and risky to the majority generally goes higher and what seems low and cheap generally goes lower."}
+,
+{"id":"26","author":"Warren Buffett","quote":"You don't need to be a rocket scientist. Investing is not a game where the guy with the 160 IQ beats the guy with 130 IQ."}
+,
+{"id":"27","author":"Jesse Livermore","quote":"Money is made by sitting, not trading."},
+{"id":"28","author":"Paul Tudor Jones","quote":"That cotton trade was almost the deal breaker for me. It was at that point that I said, 'Mr. Stupid, why risk everything on one trade? Why not make your life a pursuit of happiness rather than pain?"}
+,
+{"id":"29","author":"Ed Seykota","quote":"The elements of good trading are: (1) cutting losses, (2) cutting losses, and (3) cutting losses. If you can follow these three rules, you may have a chance."},
+{"id":"30","author":"Yvan Byeajee","quote":"Money is just something you need in case you do not die tomorrow. Let this is a reminder for you not to obsess over profits and losses. In whatever you do, strive for enjoyment, focus, contentment, humility, openness' Paradoxically (and as an unintended consequence) your trading performance will improve significantly."}
+,
+{"id":"31","author":"Yvan Byeajee","quote":"Confidence is not 'I will profit on this trade.' Confidence is 'I will be fine if I don't profit from this trade."}
+,
+{"id":"32","author":"Unknown","quote":"You don't need to trade often. If you can catch one or two moves to the targets during the day with good size, you can make a good living and keep trading costs down"}
+,
+{"id":"33","author":"Jaymin Shah","quote":"Don't blindly follow someone, follow market and try to hear what it is telling you."}
+,
+{"id":"34","author":"David Sikhosana","quote":"Losses are necessary, as long as they are associated with a technique to help you learn from them"},
+{"id":"35","author":"Larry Hite","quote":"Throughout my financial career, I have continually witnessed examples of other people that I have known being ruined by a failure to respect risk. If you don't take a hard look at risk, it will take you"}
+,
+{"id":"36","author":"Tom Basso","quote":"I think investment psychology is by far the more important element, followed by risk control, with the least important consideration being the question of where you buy and sell."},
+{"id":"37","author":"Confucius","quote":"It does not matter how slowly you go as long as you do not stop."},
+{"id":"38","author":"Alexander Elder","quote":"The goal of a successful trader is to make the best trades. Money is secondary."},
+{"id":"39","author":"Jesse Livermore","quote":"The desire for constant action irrespective of underlying conditions is responsible for many losses in Wall Street even among the professionals, who feel that they must take home some money every day, as though they were working for regular wages."},
+{"id":"40","author":"Anthony Robbins","quote":"It's not what we do once in a while that shapes our lives. It's what we do consistently."}
+,
+{"id":"41","author":"Marty Schwartz","quote":"Learn to take losses. The most important thing in making money is not letting your losses get out of hand"},
+{"id":"42","author":"Marty Schwartz","quote":"I have learned through the years that after a good run of profits in the markets, it`s very important to take a few days off as a reward. The natural tendency is to keep pushing until the streak ends. But experience has taught me that a rest in the middle of the streak can often extend it."},
+{"id":"43","author":"Peter Lynch","quote":"In this business if you're good, you're right six times out of ten. You're never going to be right nine times out of ten."}
+,
+{"id":"44","author":"Warren Buffett","quote":"It takes 20 years to build a reputation and 5 minutes to ruin it. If you think about that, you'll do things differently."}
+,
+{"id":"45","author":"Bruce Kovner","quote":"If you personalize losses, you can't trade."}
+,
+{"id":"46","author":"Paul Tudor Jones","quote":"I'm always thinking about losing money as opposed to making money. Don't focus on making money, focus on protecting what you have"}
+,
+{"id":"47","author":"Yvan Byeajee","quote":"You will never find fulfillment trading the markets if you don't learn to appreciate and be satisfied with what you already have."}
+,
+{"id":"48","author":"Unknown","quote":"Markets change their behavior faster than people can change their minds' That is why intraday trading is so difficult"}
+,
+{"id":"49","author":"Yvan Byeajee","quote":"Focus, patience, wise discernment, non-attachment 'the skills you acquire in meditation and the skills you need to thrive in trading are one and the same."}
+
+]
